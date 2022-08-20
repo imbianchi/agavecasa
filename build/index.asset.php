a7de5b2ca2d79cef198962eddea9fdd52e7d044b
Joao Bianchi <joaobianchi@live.com>
2022-08-20 13:37:24 -0400

    📦️ npm packages && build scripts

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e77742939ba4f062ab03');
---
+++ build/index.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+jQuery(document).ready(function ($) {
+  "use strict";
+
+  $.getScript('http://localhost/agave.loc/wp-content/themes/agavecasa/src/modules/search/Search.js').done(function () {
+    var search = new Search();
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
